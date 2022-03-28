bb7ef074a157bd020c6978d2544a0544c953ddda
Gukhyeon Ryu <rgh4806@naver.com>
2022-03-28 18:14:44 +0900

    Add RelayCommand and NotifiableBase.

---
+++ C#/Common/Library/Design/Command/RelayCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Library.Design.Command
+{
+    public class RelayCommand : ICommand
+    {
+        #region Event
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        #endregion
+
+        #region Field
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+        #endregion
+
+        #region Constructor
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException("execute");
+            this.canExecute = canExecute;
+        }
+        #endregion
+
+        #region Method
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+        #endregion
+    }
+
+    class RelayCommand<T> : ICommand
+    {
+        #region Event
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        #endregion
+
+        #region Field
+        private readonly Action<T> execute;
+        private readonly Predicate<T> canExecute;
+        #endregion
+
+        #region Constructor
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException("execute");
+            this.canExecute = canExecute;
+        }
+        #endregion
+
+        #region Method
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke((T)parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+        #endregion
+    }
+}
---
+++ C#/Common/Library/Design/Mvvm/NotifiableBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Library.Design.Mvvm
+{
+    [DebuggerStepThrough]
+    public class NotifiableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
